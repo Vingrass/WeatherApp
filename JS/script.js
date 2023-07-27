5c8dcb7924e923f095b2b3d9a965a1fc1b0e6f22
Vingrass <ingrassiav99@gmail.com>
2023-07-26 21:05:54 -0700

    added buttons to show past searches

--- JS/script.js
+++ JS/script.js
@@ -1,6 +1,25 @@
+//I've had a very not good week and didnt have much time to work on this. I had to rush through alot of this. I am not proud very proud of the result.
 const APIKey = "57fd6244d936d349b1e391d63d133c49"
 var city;
 var numQuests = 5;
+var btnPress = 0;
+
+var lastSearch1 = document.querySelector('#lastSearch1');
+var lastSearch2 = document.querySelector('#lastSearch2');
+var lastSearch3 = document.querySelector('#lastSearch3');
+var lastSearch4 = document.querySelector('#lastSearch4');
+var lastSearch5 = document.querySelector('#lastSearch5');
+
+
+lastSearch1.addEventListener("click",()=> buttonWeather(1))
+lastSearch2.addEventListener("click",()=> buttonWeather(2))
+lastSearch3.addEventListener("click",()=> buttonWeather(3))
+lastSearch4.addEventListener("click",()=> buttonWeather(4))
+lastSearch5.addEventListener("click",()=> buttonWeather(5))
+
+localStorage.clear();
+
+
 
 ///\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\\\
 async function getWeather(){
@@ -16,6 +35,8 @@ async function getWeather(){
     const weatherResp = await fetch(`http://api.openweathermap.org/data/2.5/forecast?lat=${cords[0].lat}&lon=${cords[0].lon}&units=imperial&appid=${APIKey}`)
     const weather = await weatherResp.json();
 
+
+    
     document.getElementById("temp1").innerHTML += weather.list[0].main.temp + " F";
     document.getElementById("hum1").innerHTML += weather.list[0].main.humidity + "%";
     document.getElementById("wind1").innerHTML += weather.list[0].wind.speed + "MPH";
@@ -36,14 +57,16 @@ async function getWeather(){
     document.getElementById("hum5").innerHTML += weather.list[32].main.humidity + "%";
     document.getElementById("wind5").innerHTML += weather.list[32].wind.speed + "MPH";
 
+    if(btnPress == 0){
+        showBtn();
+    }
 
-    showBtn();
 }
 ///\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\\\
 
 function showBtn(){
     
-    if(numQuests == 5){
+    if(numQuests == 0){
         numQuests = 5;
     }
     if(numQuests == 5){
@@ -67,10 +90,16 @@ function showBtn(){
         document.getElementById("lastSearch1").style.display = "inline";
         document.getElementById("lastSearch1").innerHTML = localStorage.getItem("cityFive");
     }
-    numQuests--;
 
+    numQuests -= 1;
+    console.log(localStorage.getItem("cityOne"));
+    console.log(localStorage.getItem("cityTwo"));
+    console.log(localStorage.getItem("cityThree"));
+    console.log(localStorage.getItem("cityFour"));
+    console.log(localStorage.getItem("cityFive"));
 }
 
+///\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\\\
 function clearWeather(){
     document.getElementById("temp1").innerHTML = "Temp: ";
     document.getElementById("hum1").innerHTML = "Humidity: ";
@@ -91,4 +120,28 @@ function clearWeather(){
     document.getElementById("temp5").innerHTML = "Temp: ";
     document.getElementById("hum5").innerHTML = "Humidity: ";
     document.getElementById("wind5").innerHTML = "Wind Speed: ";
+}
+///\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\\\
+
+function buttonWeather(btnNum){
+    btnPress = 1;
+
+    if(btnNum == 1){
+        city = localStorage.getItem("cityFive");
+        getWeather();
+    }else if(btnNum == 2){
+        city = localStorage.getItem("cityFour");
+        getWeather();
+    }else if(btnNum == 3){
+        city = localStorage.getItem("cityThree");
+        getWeather();
+    }else if(btnNum == 4){
+        city = localStorage.getItem("cityTwo");
+        getWeather();
+    }else if(btnNum == 5){
+        city = localStorage.getItem("cityOne");
+        getWeather();
+    }
+
+    btnPress = 0;
 }
